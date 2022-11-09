e52d14b01bfc5df3cf1c01fdd1c06b2e682aed52
sergeemmanuelk <serge_em@outlook.fr>
2022-11-09 11:46:45 +0000

    Feature - Add collection

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from tasks.models import Collection, Task
+
 # Register your models here.
+
+admin.site.register(Task)
+admin.site.register(Collection)
--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.utils.html import escape
+from tasks.models import Collection
 
 # Create your views here.
+
+def index(request):
+    context = {}
+    context["collections"] = Collection.objects.order_by("name")
+    
+    return render(request, 'tasks/index.html', context=context)
+
+def add_collection(request):
+    collection_name = escape(request.POST.get("collection-name"))
+    collection, created = Collection.objects.get_or_create(name=collection_name)
+    if not created:
+        return HttpResponse("La collection existe déjà.")
+    
+    return redirect("home")
