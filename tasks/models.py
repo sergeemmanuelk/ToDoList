c021027f066056798458565a74371af71d1c8606
sergeemmanuelk <serge_em@outlook.fr>
2022-11-09 10:26:34 +0000

    Make migration

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Collection(models.Model):
+    name = models.CharField(max_length=60)
+    slug = models.SlugField()
+    
+    
+class Task(models.Model):
+    description = models.CharField(max_length=300)
+    collection = models.ForeignKey(Collection, on_delete=models.CASCADE)
